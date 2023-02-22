374293bfa00a0d4976dc2dd786fff7629a46e350
upendra <ugrarapuupendra@gmail.com>
2023-02-22 13:17:22 +0530

    print color

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-
+echo -e "\e[36m installing nginx \e[0m"
 yum install nginx -y
+
+echo -e "\e[36m removing unwanted files \e[0m"
 rm -rf /usr/share/nginx/html/* 
+
+echo -e "\e[36m downloading the code \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+
+echo -e "\e[36m changing the directory \e[0m"
 cd /usr/share/nginx/html 
+
+echo -e "\e[36m unzip the code file \e[0m"
 unzip /tmp/frontend.zip
 
+echo -e "\e[36m copying the code to default location \e[0m"
 cp configs/nginx-roboshop.conf /etc/nginx/defualt.d/roboshop.conf
 
+echo -e "\e[36m enable and restart nginx \e[0m"
 systemctl enable nginx 
 systemctl restart nginx  
 
