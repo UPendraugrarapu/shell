043d67624481c095ed8c9b81ca4428d25f84c4ea
upendra <ugrarapuupendra@gmail.com>
2023-02-24 15:55:07 +0530

    replacing listen address

--- mongodb.sh
+++ mongodb.sh
@@ -6,8 +6,10 @@ cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 print_head "installing mongodb"
 yum install mongodb-org -y &>>${log_file}
 
+print_head "updating the mongodb listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
 print_head "enable and start mongodb service"
 systemctl enable mongod &>>${log_file}
 systemctl start mongod &>>${log_file}
 
-#replace 127.0.0.1 with 0.0.0.0 in vim /etc/mongod.conf
