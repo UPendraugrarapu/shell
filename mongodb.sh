7f25bec36361f2deb2396f562a50183917e8687b
upendra <ugrarapuupendra@gmail.com>
2023-02-24 13:51:23 +0530

    updating source common.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,12 @@
+source common.sh
+
+print_head "setup mongodb repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 
-
+print_head "installing mongodb"
 yum install mongodb-org -y
+
+print_head "enable and start mongodb service"
 systemctl enable mongod
 systemctl start mongod
 
