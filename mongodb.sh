b0f8f0f1983eb253b48663950a2c51a76a2e6e76
upendra <ugrarapuupendra@gmail.com>
2023-02-24 13:57:40 +0530

    export logs

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,13 @@
 source common.sh
 
 print_head "setup mongodb repository"
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "installing mongodb"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log_file}
 
 print_head "enable and start mongodb service"
-systemctl enable mongod
-systemctl start mongod
+systemctl enable mongod &>>${log_file}
+systemctl start mongod &>>${log_file}
 
 #replace 127.0.0.1 with 0.0.0.0 in vim /etc/mongod.conf
