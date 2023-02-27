d4281ae409437074777952128ed97e1886d65ebc
upendra <ugrarapuupendra@gmail.com>
2023-02-27 09:25:36 +0530

    user code update

--- user.sh
+++ user.sh
@@ -1,14 +1,56 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
-cd /app 
-unzip /tmp/user.zip
-cd /app 
-npm install 
-systemctl daemon-reload
-systemctl enable user 
-systemctl start user
-yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
+source common.sh
+
+print_head "Downloading the nodejs repo files"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_head "Installing nodejs"
+yum install nodejs -y &>>${log_file}
+status_check $?
+
+print_head "Add user"
+id roboshop &>>${log_file}
+if [ $? -ne 0 ] ; then
+ useradd roboshop &>>${log_file}
+ fi
+status_check $?
+
+print_head "Create directory"
+if [ ! -d /app ] ; then
+ mkdir /app  &>>${log_file}
+ fi
+status_check $?
+
+print_head "Removing old files"
+rm -rf /app/* &>>${log_file}
+status_check $?
+print_head "Download app content"
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip  &>>${log_file}
+status_check $?
+print_head "Change directory & extract the app content file"
+cd /app &>>${log_file}
+unzip /tmp/user.zip &>>${log_file}
+status_check $?
+print_head "change directory & install nodejs dependencies"
+cd /app &>>${log_file}
+npm install &>>${log_file}
+status_check $?
+print_head "copy the systemd service file"
+cp ${code_dir}/configs/user.service /etc/systemd/system/user.service &>>${log_file}
+status_check $?
+print_head "Reload the service"
+systemctl daemon-reload &>>${log_file}
+status_check $?
+print_head "Enable and start the service"
+systemctl enable user &>>${log_file}
+systemctl start user &>>${log_file}
+status_check $?
+print_head "copy mongodb repo file server"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+status_check $?
+print_head "install mongodb"
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+print_head "Load the schema"
+mongo --host mongodb.devopsb71.tech </app/schema/user.js &>>${log_file}
+status_check $?
