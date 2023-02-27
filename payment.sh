9e203a494295a489bbe352736d3efcdd7efced1a
upendra <ugrarapuupendra@gmail.com>
2023-02-27 13:35:20 +0530

    update

--- payment.sh
+++ payment.sh
@@ -5,7 +5,7 @@ if [ -z "${roboshop_app_password}" ] ; then
 echo -e "\e[31mMissing RabbitMQ app user password argument\e[0m"
 exit 1
 fi
-#proivde 'RoboShop@1' password at the time of executing bash payement.sh
+#proivde 'roboshop123' password at the time of executing bash payement.sh
 
 component=payement
 
