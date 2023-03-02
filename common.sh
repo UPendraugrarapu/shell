6397129e7c35bdaa1a9cdd96362ba30431179bde
upendra <ugrarapuupendra@gmail.com>
2023-03-02 12:27:46 +0530

    update

--- common.sh
+++ common.sh
@@ -146,3 +146,29 @@ python() {
 
 
 }
+
+golang() {
+
+print_head "Install golang"
+yum install golang -y &>>${log_file}
+status_check $?
+
+app_prereq_setup
+
+cd/app
+
+print_head "Download dependencies"
+go mod init dispatch &>>${log_file}
+go get  &>>${log_file}
+go build &>>${log_file}
+status_check $?
+
+print_head "Reload system"
+systemctl daemon-reload  &>>${log_file}
+status_check $?
+
+print_head "Enable and start service"
+systemctl enable dispatch &>>${log_file}
+systemctl start dispatch &>>${log_file}
+status_check $?
+}
--- dispatch.sh
+++ dispatch.sh
@@ -2,25 +2,4 @@ source common.sh
 
 component=dispatch
 
-print_head "Install golang"
-yum install golang -y &>>${log_file}
-status_check $?
-
-app_prereq_setup
-
-
-
-print_head "Download dependencies"
-go mod init dispatch &>>${log_file}
-go get  &>>${log_file}
-go build &>>${log_file}
-status_check $?
-
-print_head "Reload system"
-systemctl daemon-reload  &>>${log_file}
-status_check $?
-
-print_head "Enable and start service"
-systemctl enable dispatch &>>${log_file}
-systemctl start dispatch &>>${log_file}
-status_check $?
+golang
