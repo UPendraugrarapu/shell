6a1e6a71d56de82abe5a0b25597a439039fb64ff
upendra <ugrarapuupendra@gmail.com>
2023-03-02 11:31:50 +0530

    update

--- payment.sh
+++ payment.sh
@@ -1,9 +1,9 @@
 source common.sh
 
 roboshop_app_password=$1
-if [ -z "${roboshop_app_password}" ] ; then
-echo -e "\e[31mMissing RabbitMQ app user password argument\e[0m"
-exit 1
+if [ -z "${roboshop_app_password}" ]; then
+    echo -e "\e[31mMissing RabbitMQ app user password argument\e[0m"
+    exit 1
 fi
 #proivde 'roboshop123' password at the time of executing bash payement.sh
 
